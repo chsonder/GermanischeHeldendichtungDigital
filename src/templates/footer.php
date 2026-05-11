2d5ca03a0580812a394d1785b862ae6b99d212f2
chsonder <christian.sonder@trilog-verlag.de>
2026-05-11 23:33:33 +0200

    Update footer.php

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,6 +1,6 @@
 <footer title="Fußbereich der Seite" id="footer">
     <div title="Link zur Startseite" class="footer_spalte">
-        <h1><a class="sonstiger_link" href="../index.php?page=templates/home>Startseite</a></h1>
+        <h1><a class="sonstiger_link" href="../index.php?page=templates/home">Startseite</a></h1>
         <hr class="linie start_color"/>
         <ul class="footer_liste">
             <li><a class="sonstiger_link" href="#main">direkt zum Inhalt</a></li>
