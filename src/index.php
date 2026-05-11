b6e006fcf6613328b07dabd8b3a6e9710d7ee3a9
chsonder <christian.sonder@trilog-verlag.de>
2026-05-11 23:33:20 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -9,11 +9,7 @@ require_once 'functions.php';
     <meta charset="UTF-8"/>
     <meta name="author" content="Christian Sonder"/>
     <meta name="description" content="Germanische Heldendichtung Digital"/>
-    
-    <!-- Icon in Browser-Tabs -->
     <link rel="icon" href="assets/svg/logo_farbe.svg" title="Verlagslogo Trilog-Verlag"/>
-
-    <!-- Stylesheets -->
     <link rel="stylesheet" type="text/css" href="assets/css/0_general.css"/>
     <link rel="stylesheet" type="text/css" href="assets/css/1_small.css"/>
     <link rel="stylesheet" type="text/css" href="assets/css/2_medium.css"/>
@@ -23,16 +19,13 @@ require_once 'functions.php';
 <?php include_once 'templates/header.php';?>
 <?php include_once 'templates/navigation.php';?>
 <?php include_once 'templates/sidenavigation.php';?>
-
 <main id="main" tabindex="-1">
 <?php
 $seite = validatePage();
 include_once $seite;
 ?>
 </main>
-
 <?php include_once 'templates/aside.php';?>
 <?php include_once 'templates/footer.php'; ?>
-
 </body>
 </html>
